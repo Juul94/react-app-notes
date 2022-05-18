34ed1a90ef0ac260c76bd465a57ae5865eee88fd
Alexander Sørensen <juul.94@hotmail.com>
2022-05-18 13:37:23 +0200

    Routes modified

--- src/App.js
+++ src/App.js
@@ -22,20 +22,43 @@ function App() {
     <main>
       <Routes>
         <Route
-          path="/"
+          // FOR GB PAGES
+          // path="/"
+          path="/react-app-notes"
           element={<Login_view title="Login" setProfileData={setProfileData} />}
         />
         <Route
-          path="/register"
+          // path="/register"
+          path="/react-app-notes/register"
           element={
             <Register_view title="Register" setProfileData={setProfileData} />
           }
           exact
         />
-        <Route path="/home" element={<Home_view />} exact />
-        <Route path="/notes" element={<Notes_view />} exact />
-        <Route path="/addnote" element={<AddNote_view />} exact />
-        <Route path="/editnote" element={<EditNote_view />} exact />
+        <Route
+          // path="/home"
+          path="react-app-notes/home"
+          element={<Home_view />}
+          exact
+        />
+        <Route
+          //  path="/notes"
+          path="react-app-notes/notes"
+          element={<Notes_view />}
+          exact
+        />
+        <Route
+          // path="/addnote"
+          path="react-app-notes/addnote"
+          element={<AddNote_view />}
+          exact
+        />
+        <Route
+          // path="/editnote"
+          path="react-app-notes/editnote"
+          element={<EditNote_view />}
+          exact
+        />
       </Routes>
     </main>
   );
